1fba6911e2e53ac77f5221f2649e1f1762ef034f
Ingrid Stemer <ingrid.stemer@live.fr>
2023-05-09 10:33:53 +0200

    add pages components and create app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { NewListComponent } from './Pages/new-list/new-list.component';
+import { ListeComponent } from './Pages/liste/liste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : HomePageComponent},
+  {path : 'createListe', component : NewListComponent},
+  {path : 'liste/:id', component : ListeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
   ],
   imports: [
     CoreModule,
